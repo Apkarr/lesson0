0998d4bfe95193a28f80bbb1611bf5f15d8f33b9
Артём <97799751+Apkarr@users.noreply.github.com>
2024-06-16 11:37:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+
+a = str('Хорошая погода')
+print(len(a))
+
+
+first = 8
+second = 12
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+
+first_string = "Вторник"
+second_string = "Понедельник,"
+print(second_string, " ", first_string)
+
+
+a = 3
+b = 7
+c = -2
+f = (a * b) + (a * c)
+d = (f ** 3) / 2
+print(d)
+
